126312dbd03926037a38a5979d57db749d7b7caf
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2024-04-13 01:36:06 +0700

    add level check on logger class

--- src/Logger.php
+++ src/Logger.php
@@ -2,17 +2,20 @@
 
 namespace Iqbalatma\LaravelLogTelegramChannel;
 use Iqbalatma\LaravelLogTelegramChannel\Services\TelegramHandler;
+use Monolog\Level;
 use Monolog\Logger as MonologLogger;
 class Logger
 {
     /**
+     * @param array $configuration
      * @return MonologLogger
      */
-    public function __invoke(): MonologLogger
+    public function __invoke(array $configuration): MonologLogger
     {
+        $level = $configuration['level'] ?? Level::Debug;
         return new MonologLogger(
             "iqbalatma/laravel-log-telegram-channel",
-            [new TelegramHandler()]
+            [new TelegramHandler($level)]
         );
     }
 }
